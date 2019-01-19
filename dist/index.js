f44987d1d436c2b5abfd79909ba5b5f20888edf6
Christopher Brown <chbphone55@gmail.com>
2019-01-19 23:02:57 +0000

    switched to custom config with rollup and building and testing work!

---
+++ dist/index.js
@@ -0,0 +1,217 @@
+/* mui-phone-input version 1.0.2 */
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+var TextField = _interopDefault(require('@material-ui/core/TextField'));
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function");
+  }
+
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      writable: true,
+      configurable: true
+    }
+  });
+  if (superClass) _setPrototypeOf(subClass, superClass);
+}
+
+function _getPrototypeOf(o) {
+  _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
+    return o.__proto__ || Object.getPrototypeOf(o);
+  };
+  return _getPrototypeOf(o);
+}
+
+function _setPrototypeOf(o, p) {
+  _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+    o.__proto__ = p;
+    return o;
+  };
+
+  return _setPrototypeOf(o, p);
+}
+
+function _objectWithoutPropertiesLoose(source, excluded) {
+  if (source == null) return {};
+  var target = {};
+  var sourceKeys = Object.keys(source);
+  var key, i;
+
+  for (i = 0; i < sourceKeys.length; i++) {
+    key = sourceKeys[i];
+    if (excluded.indexOf(key) >= 0) continue;
+    target[key] = source[key];
+  }
+
+  return target;
+}
+
+function _objectWithoutProperties(source, excluded) {
+  if (source == null) return {};
+
+  var target = _objectWithoutPropertiesLoose(source, excluded);
+
+  var key, i;
+
+  if (Object.getOwnPropertySymbols) {
+    var sourceSymbolKeys = Object.getOwnPropertySymbols(source);
+
+    for (i = 0; i < sourceSymbolKeys.length; i++) {
+      key = sourceSymbolKeys[i];
+      if (excluded.indexOf(key) >= 0) continue;
+      if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue;
+      target[key] = source[key];
+    }
+  }
+
+  return target;
+}
+
+function _assertThisInitialized(self) {
+  if (self === void 0) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+  }
+
+  return self;
+}
+
+function _possibleConstructorReturn(self, call) {
+  if (call && (typeof call === "object" || typeof call === "function")) {
+    return call;
+  }
+
+  return _assertThisInitialized(self);
+}
+
+var PhoneInput =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(PhoneInput, _React$Component);
+
+  function PhoneInput() {
+    var _getPrototypeOf2;
+
+    var _this;
+
+    _classCallCheck(this, PhoneInput);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(PhoneInput)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "state", {
+      value: ''
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "handleChange", function (event) {
+      var el = event.target;
+      var selectionStart = el.selectionStart;
+      var newInput = event.target.value.replace(/\D/g, '');
+
+      var oldValueArr = _this.state.value.split('-');
+
+      var newValueArr = [newInput.slice(0, 3), newInput.slice(3, 6), newInput.slice(6, 10)].filter(function (x) {
+        return x;
+      });
+      var newValue = newValueArr.join('-');
+
+      if (_this.props.onChange) {
+        _this.props.onChange(newValue);
+      }
+
+      _this.setState({
+        value: newValue
+      }, function () {
+        selectionStart += Math.max(newValueArr.length - oldValueArr.length, 0);
+        el.selectionStart = el.selectionEnd = selectionStart;
+      });
+    });
+
+    return _this;
+  }
+
+  _createClass(PhoneInput, [{
+    key: "render",
+    value: function render() {
+      var _this$props = this.props,
+          onChange = _this$props.onChange,
+          props = _objectWithoutProperties(_this$props, ["onChange"]);
+
+      return React.createElement(TextField, _extends({
+        label: "Phone",
+        type: "tel",
+        name: "phone",
+        value: this.state.value,
+        onChange: this.handleChange
+      }, props));
+    }
+  }]);
+
+  return PhoneInput;
+}(React.Component);
+
+module.exports = PhoneInput;
+/* follow me on twitter @ChrisHBrown55 */
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,20 @@
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+import fs from 'fs';
+
+function requireJSON(filePath) {
+  return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+};
+
 export default {
   input: 'src/PhoneInput.js',
   output: {
     file: 'dist/index.js',
-    format: 'cjs'
-  }
-}
+    format: 'cjs',
+    banner: `/* mui-phone-input version ${pkg.version} */`,
+    footer: '/* follow me on twitter @ChrisHBrown55 */'
+  },
+  plugins: [
+    babel(requireJSON('./.babelrc'))
+  ]
+};
