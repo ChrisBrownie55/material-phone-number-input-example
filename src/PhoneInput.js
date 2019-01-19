dfb077036a2897b7fb125b60c940bd763f67b6f9
Christopher Brown <chbphone55@gmail.com>
2019-01-19 13:39:44 +0000

    stopped using class fields in component because CRA does not support it

--- src/PhoneInput.js
+++ src/PhoneInput.js
@@ -2,9 +2,12 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
 class PhoneInput extends React.Component {
-  state = {
-    value: ''
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+  }
 
   handleChange = event => {
     const el = event.target;
